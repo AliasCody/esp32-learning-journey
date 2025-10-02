ba0109c3c3a030a0672c2af2b16a42a4b05028ab
Yu-Cheng Wang <aliascody.en12@nycu.edu.tw>
2025-10-02 22:10:43 +0800

    3 1

--- week1_blinky/src/main.cpp
+++ week1_blinky/src/main.cpp
@@ -1,12 +1,35 @@
-#include <Arduino.h>
-const uint8_t LED_PIN = 2; // 宣告 LED_PIN 常數
+// #include <Arduino.h>
+// const uint8_t LED_PIN = 2; // 宣告 LED_PIN 常數
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
+// void setup() {
+//   pinMode(LED_PIN, OUTPUT);
+//   Serial.begin(115200);
+//   Serial.println("ESP32 Blinky Ready");
+// }
+// void loop() {
+//   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+//   delay(500);
+// }
+
+
+#include<Arduino.h>
+const uint8_t LED_PIN = 2;
+
+void setup(){
+  pinMode(LED_PIN,OUTPUT);
   Serial.begin(115200);
-  Serial.println("ESP32 Blinky Ready");
+  Serial.println("ESP32 LED Pattern Ready");
 }
-void loop() {
-  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  delay(500);
+
+void loop(){
+  // 快閃
+  for(int i=0 ; i<3 ; i++){
+    digitalWrite(LED_PIN,HIGH);
+    delay(200);
+    digitalWrite(LED_PIN,LOW);
+    delay(200);
+  }
+
+  // 停 1 秒
+  delay(1000);
 }
